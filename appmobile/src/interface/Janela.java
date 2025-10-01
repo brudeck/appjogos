e2cf7c7db272e62aea87298257f538f0dbddde49
bella brudeck <isabella.brudeck.machado@escola.pr.gov.br>
2025-10-01 09:54:26 -0300

    ii

---
+++ appmobile/src/interface/Janela.java
@@ -0,0 +1,17 @@
+
+import java.awt.CardLayout;
+import java.security.Principal;
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Janela extends JFrame{
+    public Janela(){
+        this.setBounds(0,0,1920,1080);
+        CardLayout controleTela = newCardLayout();
+        JPanel telas = JPanel(controleTela);
+        LoginPanel LoginPanel = newLoginPanel(telas, this);
+        PrincipalPanel PrincipalPanel = newPrincipalPanel(telas, this);
+    }
+}
