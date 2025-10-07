652b21585169e13cc410ce334a459e9ec7519b65
bella brudeck <isabella.brudeck.machado@escola.pr.gov.br>
2025-10-07 09:50:42 -0300

    a

--- appmobile/src/interface/Janela.java
+++ appmobile/src/interface/Janela.java
@@ -13,5 +13,12 @@ public class Janela extends JFrame{
         JPanel telas = JPanel(controleTela);
         LoginPanel LoginPanel = newLoginPanel(telas, this);
         PrincipalPanel PrincipalPanel = newPrincipalPanel(telas, this);
+
+        this.add(telas);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setUndecorated(true);
+        this.setVisible(true);
+
     }
 }
