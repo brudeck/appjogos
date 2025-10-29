023b8bbf80bd73ec1913b28134a40475b0e16013
bella brudeck <isabella.brudeck.machado@escola.pr.gov.br>
2025-10-29 09:53:23 -0300

    ,,

--- appmobile/src/teste/janela.java
+++ appmobile/src/teste/janela.java
@@ -1,11 +1,13 @@
 package teste;
 
+import interfaces.Janela;
+
 public class janela {
     public static void main(String[] args) {
         //JFrame janela = new JFrame("título da janela");
         //janela.setBounds(0,0,1920,1080);
         //janela.setVisible(true);
 
-        Janela janela = newJanela();
+        Janela janela = new Janela();
     }
 }
